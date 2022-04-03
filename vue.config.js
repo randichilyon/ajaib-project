6499a035a4e79bd995c91d331e46c4adbfdc0224
GDN\randi.alfianto <randi.alfianto@gdn-commerce.com>
2022-04-03 10:23:48 +0700

    add pages

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,21 @@ module.exports = defineConfig({
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ajaib-project/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Ajaib Project',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  }
 })
